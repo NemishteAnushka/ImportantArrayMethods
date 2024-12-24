ae4a2e31db28098a9282aacb325c908dc17569e1
NemishteAnushka <nemishte.anushka23@gmail.com>
2024-12-24 22:44:12 +0530

    fill

---
+++ fill-array-method/Example1.js
@@ -0,0 +1,13 @@
+// The fill() method fills specified elements in an array with a value.
+
+// The fill() method overwrites the original array.
+
+// Start and end position can be specified. If not, all elements will be filled.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+//fill all array elements with "kiwi"
+
+fruits.fill("kiwi");
+
+console.log(fruits);
---
+++ fill-array-method/Example2.js
@@ -0,0 +1,4 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//value start index end index
+fruits.fill("Kiwi", 2, 4);
+console.log(fruits);
